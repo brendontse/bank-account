9ba2dd7d71289c0fda1fa0274cdfc6f4344272f3
Dominic Montelongo and Kira Roberts <dominic@>
2019-08-27 10:47:17 -0700

    Add .bank function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 //business logic
+function bank(account) {
+  this.account = account
+}
+
+function account(name, balance) {
+  this.name = name,
+  this.balance = balance
+}
+
+Account.prototype.addAccount = function(account) {
+
+}
 
 //user interface
 $(document).ready(function(){
   $("form#accountForm").submit(function(event){
+    event.preventDefault();
+    var
+  });
+
+  $("form#moneyForm").submit(function(Event){
     event.preventDefault();
 
   });
